9166b40407a380ba8b8022b7ba437081ab543f94
JT-Knabenschuh <jtkdevelopments@gmail.com>
2016-11-03 23:18:08 +0100

    Add main-file

---
+++ html-element-edit.el
@@ -0,0 +1,24 @@
+;;; html-element-edit.el --- will follow
+
+;; Copyright 2016 Julian T. Knabenschuh
+
+;; Version: 0.0
+;; Author: Julian T. Knabenschuh <jtkdevelopments@gmail.com>
+;; Homepage: https://github.com/jtkDvlp/html-element-edit
+;; License: GNU General Public License >= 2
+;; Distribution: This file is not part of Emacs
+;; Keywords: html web-mode paredit
+;; Package-Requires: ((web-mode))
+
+;;; Commentary:
+
+;; will follow
+
+;;; Code:
+
+(defgroup html-element-edit nil
+  "will follow"
+  :group 'languages)
+
+(provide 'html-element-edit)
+;;; html-element-edit.el ends here
