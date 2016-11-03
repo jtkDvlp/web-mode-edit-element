376b624f988e7270e08d025b82ee4a966c8c5f0e
JT-Knabenschuh <jtkdevelopments@gmail.com>
2016-11-03 23:23:12 +0100

    Some element attribute helpers

---
+++ html-element-edit-attribute.el
@@ -0,0 +1,26 @@
+;;; html-element-edit-attributes.el
+
+;; Copyright 2016 Julian T. Knabenschuh
+
+;; Author: Julian T. Knabenschuh <jtkdevelopments@gmail.com>
+;; License: GNU General Public License >= 2
+;; Distribution: This file is not part of Emacs
+
+;; Code:
+
+(require 'web-mode)
+
+(defun html-element-edit-attributes-end-inside ()
+  (interactive)
+  (web-mode-attribute-end)
+  (backward-char))
+
+(defun html-element-edit-attributes-transpose-backward ()
+  (interactive)
+  (save-excursion
+    (web-mode-attribute-beginning)
+    (web-mode-attribute-previous)
+    (web-mode-attribute-transpose)))
+
+(provide 'html-element-edit-attributes)
+;;; html-element-edit-attributes.el ends here
