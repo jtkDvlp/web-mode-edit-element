632974abd22d68ec7745143f07f94c14d8423406
JT Knabenschuh <jtkdevelopments@gmail.com>
2016-11-06 19:01:33 +0100

    Commentary in the main file

--- html-element-edit.el
+++ html-element-edit.el
@@ -1,4 +1,4 @@
-;;; html-element-edit.el --- will follow
+;;; html-element-edit.el --- Helper-functions for attribute- and element-handling
 
 ;; Copyright 2016 Julian T. Knabenschuh
 
@@ -12,7 +12,15 @@
 
 ;;; Commentary:
 
-;; will follow
+;; HTML-Element Edit is a smart enhancement for the package web-mode inspired by the packages ParEdit and Paxedit.
+
+;; It provides a few helper-functions for attribute- and element-handling based on the functions given by web-mode. Further more it provides functions for slurping, barfing, dissolving, raising ... elements inspired by ParEdit and Paxedit. Last but not least this package includes a minor mode to provide a keymap with default bindings using commands of web-mode and this package.
+
+;; To use this package, add the following lines somewhere in you init file:
+;; (require 'html-element-edit)
+;; (add-hook 'web-mode-hook 'html-element-edit-minor-mode)
+
+;; See keymap in the main file or online https://github.com/jtkDvlp/html-element-edit
 
 ;;; Code:
 
